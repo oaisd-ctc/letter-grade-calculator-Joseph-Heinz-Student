586a96b30752cac703e2cebcdc7f8c6710eb98f3
Joseph Heinz <joheb41@student.oaisd.org>
2024-10-18 14:11:00 +0000

    added program

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number score you have in a class");
+        int score = int.Parse(Console.ReadLine());
+        string letterGrade;
+        if (score >= 100)
+        {
+            Console.WriteLine("Wow! 100... That's impressive");
+            letterGrade = "A+";
+        }
+        else if (score >= 90)
+        {
+            Console.WriteLine("Very nice!");
+            letterGrade = "A";
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("Keep it up!");
+            letterGrade = "B";
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("You got thism keep working!");
+            letterGrade = "C";
+        }
+        else if (score >= 60){
+            Console.WriteLine("Keep trying!");
+            letterGrade = "D";
+        }else {
+            Console.WriteLine("Don't give up!");
+            letterGrade = "E";
+        }
+        Console.WriteLine($"Letter Grade:{letterGrade}");
+    }
+}
